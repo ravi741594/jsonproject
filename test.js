4b5ebc503c5bbecb32e5d7b6832259e5a3c7e996
ravindra kumar <ravidiyote123@gmail.com>
2022-11-16 10:37:33 +0530

    variable name change

--- test.js
+++ test.js
@@ -2,27 +2,27 @@ const card = document.getElementById('card');
 let url = "first.json";
 fetch(url).then((response) => {
   return response.json()
-}).then((data) => {
+}).then((skudata) => {
   let html = "";
-  data.forEach(el => {
-    html += `<div class="card1 col-4 card mt-5 mb-5 bg-light">
+skudata.forEach(product => {
+    html += `<div class="card1 col-4   card mt-5 mb-5 bg-light">
                    <div class="ml-3 pl-4">
                       <div class="">
-                        <img class="image" src=${el.image} alt="imag">
+                        <img class="image" src=${product.image} alt="imag">
                       </div>
                       <div>
-                        <h2>${el.name}</h2>
+                        <h2>${product.name}</h2>
                       </div>
                       <div>
                         
-                        <h4> Model :- ${el.model}</h4>
+                        <h4> Model :- ${product.model}</h4>
                       </div>
                       <div>
-                        <h4>$${el.price}</h4>
+                        <h4>$${product.price}</h4>
                       </div>
                       <div>
                         <h3>Description :- </h3>
-                        <h4>${el.description}</h4>
+                        <h4>${product.description}</h4>
                       </div>
                     </div>
 
